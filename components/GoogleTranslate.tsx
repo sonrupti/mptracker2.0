9ada0c5075c1a815d2f587136905ef13d56c13ab
sonrupti <sudiptaswain07@gmail.com>
2026-07-20 19:56:36 +0530

    Fix Google Translate dropdown visibility on mobile

--- components/GoogleTranslate.tsx
+++ components/GoogleTranslate.tsx
@@ -10,16 +10,24 @@ declare global {
 }
 
 function resetToEnglish() {
-  document.cookie = 'googtrans=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC';
-  document.cookie = `googtrans=; path=/; domain=${window.location.hostname}; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+  document.cookie =
+    'googtrans=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC';
+
+  document.cookie =
+    `googtrans=; path=/; domain=${window.location.hostname}; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+
   window.location.reload();
 }
 
 export default function GoogleTranslate() {
+
   useEffect(() => {
+
     if (document.getElementById('google-translate-script')) return;
 
+
     window.googleTranslateElementInit = () => {
+
       new window.google.translate.TranslateElement(
         {
           pageLanguage: 'en',
@@ -28,25 +36,49 @@ export default function GoogleTranslate() {
         },
         'google_translate_element'
       );
+
     };
 
+
     const script = document.createElement('script');
+
     script.id = 'google-translate-script';
+
     script.src =
       'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+
     script.async = true;
+
     document.body.appendChild(script);
+
+
   }, []);
 
+
+
   return (
-    <div className="space-y-1.5">
-      <div id="google_translate_element" className="translate-widget" />
+    <div className="min-w-[140px] space-y-1.5">
+
+      <div
+        id="google_translate_element"
+        className="translate-widget min-h-[40px]"
+      />
+
+
       <button
         onClick={resetToEnglish}
-        className="text-[12px] font-semibold text-indigo-400 hover:text-indigo-300 transition-colors"
+        className="
+          text-[12px]
+          font-semibold
+          text-indigo-400
+          hover:text-indigo-300
+          transition-colors
+        "
       >
         Reset to English
       </button>
+
+
     </div>
   );
 }
